009a8cbe71be3e2f54e30b054bba7db9c5262340
Adnan Chaudhary <90504695+Adnan099@users.noreply.github.com>
2021-11-01 15:38:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let num = '';
+let answer = '';
+
+function add(value){
+	num += value;
+	document.getElementById('result').value = num;
+}
+
+function ans(){
+	answer = eval(num);
+	document.getElementById('result').value = answer;
+	answer = '';
+	num = '';
+}
+
+function clr(){
+	num = '';
+	document.getElementById('result').value = '';
+}
